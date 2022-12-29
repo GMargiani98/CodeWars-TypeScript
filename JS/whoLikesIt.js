56e9c3b1a148e83b7cc3ab30ee2fffc3125dcdfe
george <g.margiani98@gmail.com>
2022-12-29 15:12:18 +0400

    wholikesit

---
+++ JS/whoLikesIt.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.likes = void 0;
+const likes = (a) => {
+    const stringLength = a.length;
+    let response = '';
+    if (stringLength === 0) {
+        response = 'no one likes this';
+        return response;
+    }
+    if (stringLength === 1) {
+        response = `${a[0]} likes this`;
+        return response;
+    }
+    if (stringLength === 2) {
+        response = `${a[0]} and ${a[1]} like this`;
+        return response;
+    }
+    if (stringLength === 3) {
+        response = `${a[0]}, ${a[1]} and ${a[2]} like this`;
+        return response;
+    }
+    if (stringLength >= 4) {
+        const others = stringLength - 2;
+        response = `${a[0]}, ${a[1]} and ${others} others like this`;
+        return response;
+    }
+    return response;
+};
+exports.likes = likes;
